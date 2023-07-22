a1ebc0e5316b2dec90c1364ef800a725068c8bbb
peerapat <peerapatza401@gmail.com>
2023-07-22 15:53:23 +0700

    แก้ไข : แยกหน้าล็อกอินออกจากหน้าหลัก

--- admin/lib/main.dart
+++ admin/lib/main.dart
@@ -1,112 +1,20 @@
+import 'package:admin/Screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main(){
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
+  
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: "Admin",
       home: LoginScreen(),
-    );
-  }
-}
-
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
-
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            Image.asset(
-              "assets/images/admin.png",
-              height: 320,
-              width: 350,
-              fit: BoxFit.cover,
-            ),
-            Text(
-              "เข้าสู่ระบบ",
-              style: TextStyle(
-                fontSize: 36,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: Color(0xFFF1F1F1),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 8,
-                  horizontal: 15,
-                ),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      border: InputBorder.none, hintText: 'ชื่อผู้ใช้'),
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              decoration: BoxDecoration(
-                color: Color(0xFFF1F1F1),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 8,
-                  horizontal: 15,
-                ),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      border: InputBorder.none, hintText: 'รหัสผ่าน'),
-                ),
-              ),
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: MaterialButton(
-                    color: Color.fromARGB(252, 79, 60, 247),
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
-                      onPressed: () {},
-                      child: Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Text(
-                          'เข้าสู่ระบบ',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      )),
-                ),
-              ],
-            )
-          ],
-        ),
-      )),
     );
   }
 }
