b43565de159b4f77cacaf6420a8542d6ca4589ee
peerapat <peerapatza401@gmail.com>
2023-07-29 13:57:40 +0700

    เพิ่ม : จัดการข้อมูลผู้ใช้ ปุ่ม แก้ไข ลบ

--- admin/lib/Screens/edituser_screen.dart
+++ admin/lib/Screens/edituser_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'home_screen.dart';
+
 void main() {
   runApp(EditUser());
 }
 
 class EditUser extends StatefulWidget {
-  const EditUser({super.key});
+  const EditUser({Key? key}) : super(key: key);
 
   @override
   State<EditUser> createState() => _EditUserState();
@@ -32,6 +34,68 @@ class _EditUserState extends State<EditUser> {
                 height: 100,
               ),
             ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text(
+            "หมายเลขผู้ใช้ : 0001",
+            style: TextStyle(fontSize: 25),
+          ),
+          Text(
+            "ชื่อ : พีรพัฒน์",
+            style: TextStyle(fontSize: 25),
+          ),
+          Text(
+            "นามสกุล : ตั้งปัญญาไว",
+            style: TextStyle(fontSize: 25),
+          ),
+          Text(
+            "เพศ : ชาย",
+            style: TextStyle(fontSize: 25),
+          ),
+          Text(
+            "วันเกิด : 16 ธันวาคม 2000",
+            style: TextStyle(fontSize: 25),
+          ),
+          Text(
+            "อีเมล : peerapatza401@gmail.com",
+            style: TextStyle(fontSize: 25),
+          ),
+          SizedBox(
+            height: 200,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    // โค้ดที่ต้องการให้ทำเมื่อกดปุ่ม "แก้ไข"
+                  },
+                  icon: Icon(Icons.edit),
+                  label: Text("แก้ไข"),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.yellow,
+                  ),
+                ),
+              ),
+              SizedBox(width: 10), // ระยะห่างระหว่างปุ่ม "แก้ไข" และ "ลบ"
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    // โค้ดที่ต้องการให้ทำเมื่อกดปุ่ม "ลบ"
+                  },
+                  icon: Icon(Icons.delete),
+                  label: Text("ลบ"),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
