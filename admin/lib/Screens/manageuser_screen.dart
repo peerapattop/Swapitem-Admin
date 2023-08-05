dde73fa78103a8dd03a4a8541539204e2eb56500
peerapat <peerapatza401@gmail.com>
2023-08-05 19:32:01 +0700

    เพิ่ม : แสดงข้อมูล

--- admin/lib/Screens/manageuser_screen.dart
+++ admin/lib/Screens/manageuser_screen.dart
@@ -1,11 +1,11 @@
-
 import 'package:admin/Screens/viewuser_screen.dart';
 import 'package:flutter/material.dart';
 
 import '../ScreensForHome/home_screen.dart';
 import '../ScreensForHome/notice_screen.dart';
 import '../ScreensForHome/setting_screen.dart';
-
+// ignore: depend_on_referenced_packages
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
   runApp(MyApp());
@@ -45,85 +45,80 @@ class _ManageUserState extends State<ManageUser> {
         title: Text("จัดการข้อมูลผู้ใช้"),
         centerTitle: true,
       ),
-      body: ListView(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(20.0),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                  borderSide: BorderSide(width: 0.8),
-                ),
-                hintText: "ค้นหา",
-                prefixIcon: Icon(
-                  Icons.search,
-                  size: 30,
-                ),
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    Icons.clear,
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('users').snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: Text("ไม่มีข้อมูล"),
+              );
+            }
+            return ListView(
+              children: snapshot.data!.docs.map((document) {
+                return Container(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      child: FittedBox(
+                        child: Text(document["fname"]),
+                      ),
+                    ),  
                   ),
-                  onPressed: () {},
-                ),
-              ),
-            ),
-          ),
-          createDataTable(context,"650305", "พีรพัฒน์ ตั้งปัญญาไว"),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: (index) {
-          setState(() {
-            myIndex = index;
-          });
-        },
-        currentIndex: myIndex,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'หน้าแรก'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.notifications_active), label: 'แจ้งเตือน'),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'ตั้งค่า'),
-        ],
-      ),
+                );
+              }).toList(),
+            );
+          }),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   onTap: (index) {
+      //     setState(() {
+      //       myIndex = index;
+      //     });
+      //   },
+      //   currentIndex: myIndex,
+      //   items: const [
+      //     BottomNavigationBarItem(icon: Icon(Icons.home), label: 'หน้าแรก'),
+      //     BottomNavigationBarItem(
+      //         icon: Icon(Icons.notifications_active), label: 'แจ้งเตือน'),
+      //     BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'ตั้งค่า'),
+      //   ],
+      // ),
     );
   }
 }
 
-DataTable createDataTable(BuildContext context, String id, String name) {
-  return DataTable(
-    columnSpacing: 10,
-    columns: const [
-      DataColumn(label: Text("รหัสผู้ใช้")),
-      DataColumn(label: Text("ชื่อ-นามสกุล")),
-      DataColumn(label: Text("รายละเอียด")),
-    ],
-    rows: [
-      DataRow(
-        cells: [
-          DataCell(Text(id)),
-          DataCell(Text(name)),
-          DataCell(
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const ViewUser()),
-                );
-              },
-              child: Image.asset(
-                "assets/icons/search.png",
-                width: 20,
-                height: 20,
-              ),
-              style: ElevatedButton.styleFrom(
-                primary: Colors.grey,
-                fixedSize: Size(35, 20),
-              ),
-            ),
-          ),
-        ],
-      ),
-    ],
-  );
-}
+// DataTable createDataTable(BuildContext context, String id, String name) {
+//   return DataTable(
+//     columnSpacing: 10,
+//     columns: const [
+//       DataColumn(label: Text("รหัสผู้ใช้")),
+//       DataColumn(label: Text("ชื่อ-นามสกุล")),
+//       DataColumn(label: Text("รายละเอียด")),
+//     ],
+//     rows: [
+//       DataRow(
+//         cells: [
+//           DataCell(Text(id)),
+//           DataCell(Text(name)),
+//           DataCell(
+//             ElevatedButton(
+//               onPressed: () {
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(builder: (context) => const ViewUser()),
+//                 );
+//               },
+//               child: Image.asset(
+//                 "assets/icons/search.png",
+//                 width: 20,
+//                 height: 20,
+//               ),
+//               style: ElevatedButton.styleFrom(
+//                 primary: Colors.grey,
+//                 fixedSize: Size(35, 20),
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     ],
+//   );
+// }
