e029e4ad64f0f8851600717927f088155fb8ca9c
peerapat <peerapatza401@gmail.com>
2023-08-12 19:34:00 +0700

    แก้ไข : จัดเรียง output ใหม่

--- admin/lib/Screens/showdatauser_screen.dart
+++ admin/lib/Screens/showdatauser_screen.dart
@@ -11,14 +11,22 @@ class ShowDataUser extends StatefulWidget {
 }
 
 class _UserDetailPageState extends State<ShowDataUser> {
-  late String username;
+  late String? username;
   late String email;
+  late String fname;
+  late String lname;
+  late String gender;
+  late String date;
 
   @override
   void initState() {
     super.initState();
     username = widget.userDocument['username'];
     email = widget.userDocument['email'];
+    fname = widget.userDocument['fname'];
+    lname = widget.userDocument['lname'];
+    gender = widget.userDocument['gender'];
+    date = widget.userDocument['date'];
   }
 
   @override
@@ -26,64 +34,148 @@ class _UserDetailPageState extends State<ShowDataUser> {
     return Scaffold(
       appBar: AppBar(
         title: Text("รายละเอียดผู้ใช้"),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: TextField(
-                controller: TextEditingController(text: username),
-                decoration: InputDecoration(labelText: "ชื่อผู้ใช้"),
-                onChanged: (value) {
-                  setState(() {
-                    username = value;
-                  });
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: TextField(
-                controller: TextEditingController(text: email),
-                decoration: InputDecoration(labelText: "อีเมล"),
-                onChanged: (value) {
-                  setState(() {
-                    email = value;
-                  });
-                },
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton.icon(
-                  icon: Icon(Icons.save),
-                  onPressed: () {
-                    // ดำเนินการบันทึกการแก้ไขข้อมูลได้ตรงนี้
-                  },
-                  label: Text("บันทึกการแก้ไข"),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.green, // สีพื้นหลังของปุ่ม
-                    onPrimary: Colors.white, // สีตัวอักษรในปุ่ม
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: TextField(
+                  controller: TextEditingController(text: username),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'ชื่อผู้ใช้', // ตรงนี้เป็น labelText
                   ),
-                ),
-                SizedBox(width: 15,),
-                ElevatedButton.icon(
-                  icon: Icon(Icons.delete),
-                  onPressed: () {
-                    // ดำเนินการลบข้อมูลได้ตรงนี้
+                  onChanged: (value) {
+                    setState(() {
+                      username = value;
+                    });
                   },
-                  label: Text("ลบข้อมูล"),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.red, // สีพื้นหลังของปุ่ม
-                    onPrimary: Colors.white, // สีตัวอักษรในปุ่ม
-                  ),
                 ),
-              ],
-            ),
-          ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: TextField(
+                  controller: TextEditingController(text: email),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'อีเมล', // ตรงนี้เป็น labelText
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      email = value;
+                    });
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: TextField(
+                  controller: TextEditingController(text: fname),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'ชื่อ',
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      fname = value;
+                    });
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: TextField(
+                  controller: TextEditingController(text: lname),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'นามสกุล', // ตรงนี้เป็น labelText
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      lname = value;
+                    });
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 16),
+                child: Text("เพศ",style: TextStyle(fontSize: 25),),
+              ),
+              ListTile(
+                title: const Text('ชาย'),
+                leading: Radio(
+                  value: 'ชาย',
+                  groupValue: gender,
+                  onChanged: (value) {
+                    setState(() {
+                      gender = value.toString();
+                    });
+                  },
+                ),
+              ),
+              ListTile(
+                title: const Text('หญิง'),
+                leading: Radio(
+                  value: 'หญิง',
+                  groupValue: gender,
+                  onChanged: (value) {
+                    setState(() {
+                      gender = value.toString();
+                    });
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: TextField(
+                  controller: TextEditingController(text: date),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'วันเกิด', // ตรงนี้เป็น labelText
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      date = value;
+                    });
+                  },
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton.icon(
+                    icon: Icon(Icons.save),
+                    onPressed: () {
+                      // ดำเนินการบันทึกการแก้ไขข้อมูลได้ตรงนี้
+                    },
+                    label: Text("บันทึกการแก้ไข"),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.green, // สีพื้นหลังของปุ่ม
+                      onPrimary: Colors.white, // สีตัวอักษรในปุ่ม
+                    ),
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  ElevatedButton.icon(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      // ดำเนินการลบข้อมูลได้ตรงนี้
+                    },
+                    label: Text("ลบข้อมูล"),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.red, // สีพื้นหลังของปุ่ม
+                      onPrimary: Colors.white, // สีตัวอักษรในปุ่ม
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
