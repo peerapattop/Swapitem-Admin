3918021d086063f7ce893a49fe47eeda9ad1f113
peerapattop <peerapatza401@gmail.com>
2023-12-30 15:22:00 +0700

    เพิ่มระบบการแจ้งเตือน

--- admin/lib/Screens/Manage_Screens/managenotice_screen.dart
+++ admin/lib/Screens/Manage_Screens/managenotice_screen.dart
@@ -13,54 +13,62 @@ class ViewNotice extends StatefulWidget {
 }
 
 class _ViewNoticeState extends State<ViewNotice> {
-  final notification = TextEditingController();
+  final notificationController = TextEditingController();
+  final detailCollection =
+      FirebaseFirestore.instance.collection('notifications');
 
-  final detail = FirebaseFirestore.instance.collection('notifications');
+  Future<void> createNotification() async {
+    try {
+      DateTime now = DateTime.now();
+      String formattedDate =
+          "${now.year}-${now.month.toString().padLeft(2, '0')}-${now.day.toString().padLeft(2, '0')}";
+      String formattedTime =
+          "${now.hour.toString().padLeft(2, '0')}:${now.minute.toString().padLeft(2, '0')}:${now.second.toString().padLeft(2, '0')}";
 
-  Future<void> createNotification(TextEditingController notification) async {
-  try {
-    DateTime now = DateTime.now();
-    String formattedDate = "${now.year}-${now.month.toString().padLeft(2, '0')}-${now.day.toString().padLeft(2, '0')}";
-    String formattedTime = "${now.hour.toString().padLeft(2, '0')}:${now.minute.toString().padLeft(2, '0')}:${now.second.toString().padLeft(2, '0')}";
+      // Add the notification details to Firestore
+      await detailCollection.add({
+        "รายละเอียด": notificationController.text,
+        "วันที่": formattedDate,
+        "เวลา": formattedTime,
+      });
 
-    // Add the notification details to Firestore
-    await detail.add({
-      "รายละเอียด": notification.text,
-      "วันที่": formattedDate,
-      "เวลา": formattedTime,
-    });
+      // Construct the notification message
+      var fcmMessage = {
+        "notification": {
+          "title": "New Notification",
+          "body": notificationController.text,
+        },
+        "priority": "high",
+        "data": {
+          "click_action": "FLUTTER_NOTIFICATION_CLICK",
+          "id": "1",
+          "status": "done",
+        },
+        "to":
+            "/topics/all", // Assuming you want to send to all users subscribed to the 'all' topic
+      };
 
-    // Construct the notification message
-    var fcmMessage = {
-      "notification": {
-        "title": "New Notification",
-        "body": notification.text,
-      },
-      "priority": "high",
-      "data": {
-        "click_action": "FLUTTER_NOTIFICATION_CLICK",
-        "id": "1",
-        "status": "done",
-      },
-      "to": "/topics/all", // Assuming you want to send to all users subscribed to the 'all' topic
-    };
+      // Send the notification message to FCM
+      var response = await http.post(
+        Uri.parse('https://fcm.googleapis.com/fcm/send'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization':
+              'Bearer AAAAR-udywc:APA91bExhUWIzezQhb5jmkFr8j7YP3AUip3LQJnDx3KJQcl01ObK4JPqtcZFBaLRDstq8wkI8NNMYf9eX87qsicVOUUm1e3agQY8gNZqMmXZVD7xQMHMhvHJXiH0BJgsDYCZbJSBTKC5',
+        },
+        body: jsonEncode(fcmMessage),
+      );
 
-    // Send the notification message to FCM
-    var response = await http.post(
-      Uri.parse('https://fcm.googleapis.com/fcm/send'),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'AAAAR-udywc:APA91bGdzs6jsxwDPwOjAizowJRfa09c2FgOw4VxabbPKmoftcBSm8BahlBkkrGm8znVYaSUsab2_TpFsbwBIBPMKXQEyL1k0OrmnjPRNtx2H7hSCEo3fY5CDPbhRYHZRyHqqmxX8oK8', // Replace with your server key from Firebase Console
-      },
-      body: jsonEncode(fcmMessage),
-    );
-
-    print("FCM request for device sent!");
-  } catch (e) {
-    print('Error: $e');
+      if (response.statusCode == 200) {
+        print('FCM request for device sent successfully!');
+      } else {
+        print('FCM request failed with status code: ${response.statusCode}');
+        print('Response body: ${response.body}');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
   }
-}
-
 
   @override
   Widget build(BuildContext context) {
@@ -71,10 +79,10 @@ class _ViewNoticeState extends State<ViewNotice> {
           padding: const EdgeInsets.only(top: 20.0, left: 20.0, right: 20),
           child: Column(
             children: [
-              Image.asset('assets/images/Megaphone.png',width: 250,),
-              SizedBox(height: 40,),
+              Image.asset('assets/images/Megaphone.png', width: 250),
+              SizedBox(height: 40),
               TextField(
-                controller: notification,
+                controller: notificationController,
                 maxLines: 3,
                 decoration: InputDecoration(
                   hintText: 'กรอกข้อความที่นี่',
@@ -89,20 +97,22 @@ class _ViewNoticeState extends State<ViewNotice> {
               SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () async {
-                  await createNotification(notification);
+                  await createNotification();
                   Navigator.pop(context);
                 },
                 child: Container(
-                    width: 90,
-                    height: 40,
-                    child: Center(
-                        child: Text(
+                  width: 90,
+                  height: 40,
+                  child: Center(
+                    child: Text(
                       'ส่ง',
                       style: TextStyle(fontSize: 20),
-                    ))),
+                    ),
+                  ),
+                ),
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.green, // สีพื้นหลังของปุ่ม
-                  foregroundColor: Colors.white, // สีตัวอักษรในปุ่ม
+                  backgroundColor: Colors.green,
+                  foregroundColor: Colors.white,
                 ),
               ),
             ],
